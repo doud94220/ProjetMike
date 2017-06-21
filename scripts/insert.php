72731bc6a8fa37eeb444a8e845a5f2b18321b9cc
Dimitri-M <dimitri.malvarez@hotmail.fr>
2017-06-21 11:33:03 +0200

    nom bdd

--- scripts/insert.php
+++ scripts/insert.php
@@ -2,7 +2,7 @@
 
 try{
 		$pdo = new PDO
-		('mysql:host=localhost;dbname=MikeMonRoi','root','', 
+		('mysql:host=localhost;dbname=mikemonroi','root','', 
 		array(
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, 
 			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
